f79534e4a4305ab79932982c0fce4483669a7f7d
Sparkles <smcgarr13@yahoo.com>
2023-01-18 22:15:16 -0600

    file-updates-3

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,15 @@ var city = "Los Angeles";
 
 // api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}
 
-href="https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}";
+// href="https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}";
+
+// href="https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={APIKey}";
+
+href="https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid={APIKey}&units=imperial";
+
+
+
+
 
 // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
@@ -12,13 +20,17 @@ href="https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}";
 // }) .then((data) => {
 //     console.log(data)
 // })
-
+// cityData = ({
+//    temp: data.results[0].data.temp,
+//    wind: data.results[0].data.wind,
+//    humidity: data.results[0].data.humidity,
+//  })
 
 
 
 // var weatherDetails = document.querySelector("#weather-details")
 
- $(document).ready(function () {
+ $(document).ready(function() {
     // add listner for click events
     var searchButton = $("#search-button")
     searchButton.on("click", getWeather)
@@ -31,13 +43,55 @@ function getWeather(event) {
 city = $("#city-name").val();
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+
 fetch(queryURL).then((response) => {
    return response.json()
 }) .then((data) => {
    console.log(data)
    var cityDate = $("#city-date");
-   cityDate.text(data.name);
+   cityDate.text(data.name + data.timezone);
 })
+
+// fetch(queryURL).then((response) => {
+//    return response.json()
+// }) .then((data) => {
+//    console.log(data.weather)
+//    var listGroup = $("#list-group");
+//    listGroup.text(data.weather);
+// })
+
+fetch(queryURL).then((response) => {
+   return response.json()
+}) .then((data) => {
+   console.log(data.main.temp)
+   var cityTemp = $("#city-temp");
+   cityTemp.text("Temp: " + data.main.temp);
+})
+
+fetch(queryURL).then((response) => {
+   return response.json()
+}) .then((data) => {
+   console.log(data.wind)
+   var cityWind = $("#city-wind");
+   cityWind.text("Wind: " + data.wind.speed);
+})
+
+fetch(queryURL).then((response) => {
+   return response.json()
+}) .then((data) => {
+   console.log(data.main.humidity)
+   var cityHumidity = $("#city-humidity");
+   cityHumidity.text("Humidity: " + data.main.humidity);
+})
+
+fetch(queryURL).then((response) => {
+   return response.json()
+}) .then((data) => {
+   console.log(data.main.humidity)
+   var fiveDayForecast = $("#5-Day-Forecast");
+   fiveDayForecast.text("5-day Forecast: " + data.cnt);
+})
+
 }
  });
   // get user input that was saved in localStorage
